1d1da51a2052e9d8c8783465e5d8e46a0f5e766d
yuki3738 <y.minamiya@gmail.com>
2019-06-26 21:33:52 +0900

    4.6.3 まで

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,5 +1,16 @@
-  def to_hex(r, g, b)
-    [r, g, b].inject('#') do |hex, n|
-      hex += n.to_s(16).rjust(2, '0')
-    end
+def to_hex(r, g, b)
+  [r, g, b].inject('#') do |hex, n|
+    hex += n.to_s(16).rjust(2, '0')
   end
+end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints = []
+  [r, g, b].each do |s|
+    ints << s.hex
+  end
+  ints
+end
